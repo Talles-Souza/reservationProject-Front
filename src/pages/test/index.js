8490fef57e7739f5a4297f8a64fabdd5fbaa742e
Talles Dias <tallesdelfino1@gmail.com>
2022-08-06 23:09:52 -0300

    ajustando nav bar

--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { Header } from "../../components/header";
 
 function Test() {
-
+    return (
+        <Header />
+    );
 }
- export default Test;
+export default Test;
 
---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../pages/home";
+import Test from "../pages/test";
+
+
+export default function Root() {
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/test" element={<Test />} />
+            </Routes>
+        </BrowserRouter>
+
+    )
+}
