dc6e9d1949870728b3c741e5e0801b3c6e63531c
Fernanda Machado <nandavalle2012@hotmail.com>
2022-08-08 14:09:39 -0300

    conflito

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,17 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/home";
 import Test from "../pages/test";
 
 
-export default function Root () {
-    
-    return (
-        <Routes>
-        <Route exact path="/" element={<Home/>} />
-        <Route exact path="/test" element={<Test/>} />
-        </Routes>
+export default function Root() {
 
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/test" element={<Test />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
