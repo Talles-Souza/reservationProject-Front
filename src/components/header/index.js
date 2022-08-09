1ffc03391983570ffea0f7be3b9e8d181ed9db09
Talles Dias <tallesdelfino1@gmail.com>
2022-08-09 17:56:03 -0300

    ajustes

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,24 +1,25 @@
 import React from "react";
-import Nav from 'react-bootstrap/Nav';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 
 export function Header() {
-    return (<>
-        <Nav className="justify-content-center" activeKey="/home" style={{ display: 'flex', gap: '5rem' }}>
-            <Nav.Item style={{ display: 'flex', marginTop: '1rem', gap: '5rem' }} >
-                <Nav.Link href="/home">Active</Nav.Link>
-            </Nav.Item >
-            <Nav.Item style={{ display: 'flex', marginTop: '1rem', gap: '5rem' }}>
-                <Nav.Link eventKey="link-1">Link</Nav.Link>
-            </Nav.Item>
-            <Nav.Item style={{ display: 'flex', marginTop: '1rem', gap: '5rem' }}>
-                <Nav.Link eventKey="link-2">Link</Nav.Link>
-            </Nav.Item>
-        </Nav>
-    </>
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container style={{ justifyContent: 'center' }}>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center" style={{ justifyContent: 'center' }}>
+                    <Nav style={{ gap: '5rem' }} >
+                        <Nav.Link href="#home" >Curiosidades</Nav.Link>
+                        <Nav.Link href="#link">Sobre</Nav.Link>
+                        <Nav.Link href="#link">Contatos</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
 
--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -3,9 +3,15 @@ import { Header } from "../../components/header";
 
 function Test() {
     return (<>
-       
-        <div style={{ backgroundColor: '#333', height: '100vh' }}>
-        <Header />
+
+        <div style={{ backgroundColor: "#7D7E81", height: '100vh', display: 'flex', width: '100%' }}>
+            <div style={{ backgroundColor: "#7D7E81", height: '100%', display: 'flex', width: '100%', justifyContent: "space-between", flexWrap: 'wrap' }}>
+                <div style={{ backgroundColor: "#FFFFFF", width: '39%', height: '50%' }}></div>
+                <div style={{ height: '12%', width: '60%' }}>
+                    <Header />
+                    <hr style={{ color: "#FFFFFF" }}></hr>
+                </div>
+            </div>
         </div>
     </>
     );
