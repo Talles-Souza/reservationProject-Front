784391c7134633a849a88fbedad24b99ef843596
Fernanda Machado <nandavalle2012@hotmail.com>
2022-08-09 17:55:28 -0300

    colocando cards e os butoes fixos

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,19 +1,36 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import BasicCard from "../../components/cardsHome";
 import "./style.css";
 
 function Home() {
 
-    return(
+    const history = useNavigate();
+
+    return (
         <>
-        <div className="global">
-            <div className="container">
-                <div className="bg"></div>
+            <div className="global">
+                <div className="container">
+                    <div className="cardsArea">
+                        <BasicCard /> <BasicCard /></div>
+                </div>
+                <div className="rightButtons">
+                    <div className="fixedButton1" onClick={() => history("/test")}>
+                        <text>teste1</text>
+                    </div>
+                    <div className="fixedButton2" onClick={() => history("/test")}>
+                        <text>teste2</text>
+                    </div>
+                    <div className="fixedButton3" onClick={() => history("/test")}>
+                        <text>teste3</text>
+                    </div>
+                </div>
+                <footer></footer>
             </div>
-            <footer></footer>
-        </div>
-        {/* <div className="footer"></div> */}
+
+            {/* <div className="footer"></div> */}
         </>
     );
 }
- export default Home;
+export default Home;
 
