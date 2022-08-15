91d363f26aa530b8248fdb260de148e2bf55069d
Fernanda Machado <nandavalle2012@hotmail.com>
2022-08-15 01:21:58 -0300

    fazendo alterar o modal

--- src/components/modalLogin/index.js
+++ src/components/modalLogin/index.js
@@ -1,31 +1,115 @@
-import Button from 'react-bootstrap/Button';
+import React, {useState} from 'react';
 import Modal from 'react-bootstrap/Modal';
+import Logo from "../../assets/img/logo.jpg"
+import Box from '@mui/material/Box';
+import Input from '@mui/material/Input';
+import InputAdornment from '@mui/material/InputAdornment';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import "./style.css";
 
-export const ModalSingIn =(props) =>{
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-  );
+export const ModalSingIn = (props) => {
+
+    const [steps, setSteps] = useState(0)
+    const [isClosed, setClosed] = useState(false)
+
+    // function handleClickCloseModal () {
+    //     if (isClosed === true) {
+    //         steps(e => 0)
+    //     }
+    //     setClosed(true)
+    //     }
+
+    const [values, setValues] = React.useState({
+        password: '',
+        showPassword: false,
+    });
+
+    const handleChange = (prop) => (event) => {
+        setValues({ ...values, [prop]: event.target.value });
+    };
+
+    const handleClickShowPassword = () => {
+        setValues({
+            ...values,
+            showPassword: !values.showPassword,
+        });
+    };
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
+    
+
+
+    return (
+        <Modal
+            {...props}
+            size="sm"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton onClick={e => setSteps( 0)}></Modal.Header>
+            <Modal.Title style={{ display: "flex", alignItems: "center", justifyContent: "center",  position: "relative", top:"7px" }} id="contained-modal-title-vcenter">
+                <img src={Logo}  />
+            </Modal.Title>
+
+            {steps === 0 ? <Modal.Body style={{display: "flex", justifyContent: "center", flexDirection:"column"}}>
+                <Box sx={{ display: 'flex', alignItems: 'flex-end', justifyContent: "center", width: "100%" }}>
+                    {/* <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} /> */}
+                    <TextField id="input-with-sx" label="Email" variant="standard" sx={{ width: '80%' }} />
+                </Box> 
+
+                <FormControl sx={{ display: 'flex', width: '82%', justifyContent: "center", position: "relative", left: '26px' }} variant="standard">
+                    <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+                    <Input
+                        id="standard-adornment-password"
+                        type={values.showPassword ? 'text' : 'password'}
+                        value={values.password}
+                        onChange={handleChange('password')}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                >
+                                    {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                    />
+                </FormControl>
+
+                <div className='buttonSingIn'>
+                        <text>Sing In</text>
+                </div>
+                <div className='forgetPassword'>
+                        <button onClick={e => setSteps( 1)}>Esqueceu sua senha?</button>
+                </div> 
+
+            </Modal.Body>  : <div></div>}
+
+
+            {steps === 1 ? <Modal.Body style={{display: "flex", justifyContent: "center", flexDirection:"column"}}>
+                <Box sx={{ display: 'flex', alignItems: 'flex-end', justifyContent: "center", width: "100%" }}>
+                    {/* <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} /> */}
+                    <TextField id="input-with-sx" label="Email" variant="standard" sx={{ width: '80%' }} />
+                </Box> 
+
+                <div className='buttonSingIn'>
+                        <text>Recuperar</text>
+                </div>
+
+            </Modal.Body>  : <div></div>}
+            <Modal.Footer>
+            </Modal.Footer>
+        </Modal>
+    );
 }
 
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import { useNavigate } from "react-router-dom";
 import BasicCard from "../../components/cardsHome";
 import { ModalSingIn } from "../../components/modalLogin";
-
 import "./style.css";
+
+import Box from '@mui/material/Box';
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
+import SaveIcon from '@mui/icons-material/Save';
+import PrintIcon from '@mui/icons-material/Print';
+import ShareIcon from '@mui/icons-material/Share';
 
 function Home() {
 
     const history = useNavigate();
     const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
+    const [isPageSelected, setPageSelected] = useState(false)
+
+    // const handleOpen = () => setOpen(true);
+    // const handleClose = () => setOpen(false);
+
+    const actions = [
+        { icon: <FileCopyIcon />, name: 'login' },
+        { icon: <SaveIcon />, name: 'Save' },
+        { icon: <PrintIcon />, name: 'Print' },
+        { icon: <ShareIcon />, name: 'Share' },
+    ];
 
 
     return (
@@ -33,6 +50,25 @@ function Home() {
                 </div>
                 <footer></footer>
             </div>
+
+            <Box sx={{ height: 320, transform: 'translateZ(0px)', flexGrow: 1 }}>
+                <SpeedDial 
+                    ariaLabel="SpeedDial basic example"
+                    sx={{ position: 'fixed', bottom: '350px', right: '20px' }}
+                    icon={<SpeedDialIcon />}
+                >
+                    {actions.map((action) => (
+                        <SpeedDialAction
+                            key={action.name}
+                            icon={action.icon}
+                            tooltipTitle={action.name}
+                            onClick={() => setOpen(true)}
+                        />
+                        
+                        
+                    ))} 
+                </SpeedDial>
+            </Box>
             <ModalSingIn
                 show={open}
                 onHide={() => setOpen(false)}
